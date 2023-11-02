c02d05aa75c76642b6e0b16187bf74f8ec9838f5
shreyanzjain <shreyanz32@gmail.com>
2023-11-02 22:11:36 +0530

    feat: show nodemcu status on the navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,9 @@ import TiltData from "./components/TiltData";
 // import savedData from "./components/Data.json";
 
 function App() {
-  const URL = "192.168.0.102"; // Your NodeMCU IP ADDRESS
+  const URL = "192.168.181.69"; // Your NodeMCU IP ADDRESS
   const [data, setData] = useState([]);
+  const [live, setLive] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(async () => {
@@ -16,6 +17,9 @@ function App() {
         .get(`http://${URL}:8000/get/readings`)
         .then((res) => {
           console.log(res.data);
+          if (live == false) {
+            setLive(true);
+          }
           setData((oldData) => {
             let newData = [...oldData, res.data];
             if (newData.length > 50) {
@@ -25,24 +29,38 @@ function App() {
           });
         })
         .catch((err) => {
+          if (live == true) {
+            setLive(false);
+          }
           console.log(err);
         });
-    }, 10);
+    }, 100);
 
     return () => clearInterval(interval);
-  }, []);
+  });
 
   console.log(data);
 
   return (
-    <div className="container-lg h-screen w-full bg-slate-200">
-      <div className="flex w-full h-1/2 items-center justify-center gap-8">
-        <GasData inputData={data} />
-        <DistanceData inputData={data} />
+    <div className="container-lg h-auto w-full pt-16">
+      <div className="container-lg w-full h-16 bg-eerieblack fixed top-0 z-10">
+        <div className="flex w-full h-full items-center justify-between">
+          <p className="text-eggshell text-2xl font-semibold ms-2">
+            Manhole Monitoring System
+          </p>
+          {!live && <a className="border-2 border-red-400 me-2 px-2 rounded-md text-red-400">Offline</a>}
+          {live && <a className="border-2 border-green-400 me-2 px-2 rounded-md text-green-400">Online</a>}
+        </div>
       </div>
+      <div className="container-lg h-screen bg-eggshell">
+        <div className="flex w-full h-1/2 items-center justify-center gap-8">
+          <GasData inputData={data} />
+          <DistanceData inputData={data} />
+        </div>
 
-      <div className="flex w-full h-1/2 items-center justify-center">
-        <TiltData inputData={data} />
+        <div className="flex w-full h-1/2 items-center justify-center">
+          <TiltData inputData={data} />
+        </div>
       </div>
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        platinum: "#D1DEDE",
+        eerieblack: "#1D201F",
+        oldrose: "#C58882",
+        lightcoral: "#df928e",
+        wheat: "#EAD2AC",
+        eggshell: "#F1E7D5"
+      }
+    },
   },
   plugins: [],
 }
